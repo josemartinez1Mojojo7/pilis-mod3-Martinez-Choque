91a672fd9c124a8782be1130b4e1e849109c2497
Jose Martinez <joariel17@gmail.com>
2022-10-15 04:21:14 -0300

    readme

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
 import * as bootstrap from "bootstrap";
@@ -13,13 +13,13 @@ import { UserProvider } from "./context/UserContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UserProvider>
         <ClimaProvider>
           <App />
         </ClimaProvider>
       </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
